98af9f7b7daada36f83a076800a453f0dbc7e208
Jose Adrian Marin <jose.adrian.marin@correounivalle.edu.co>
2025-11-28 22:54:05 -0500

    Created first version of tw-tasks

--- constructor.sh
+++ constructor.sh
@@ -6,19 +6,31 @@ cd ..
 
 cd TW-auth
 docker build -t tw-auth:latest .
-
 cd ..
+
 cd TW-chat
 docker build -t tw-chat:latest .
+cd ..
+
+cd TW-docs
+docker build -t tw-docs:latest .
+cd ..
+
+cd TW-tasks
+docker build -t tw-tasks:latest .
 
 cd ..
 kubectl apply -f k8s/shell.yaml
 kubectl apply -f k8s/tw-auth.yaml
 kubectl apply -f k8s/tw-chat.yaml
+kubectl apply -f k8s/tw-docs.yaml
+kubectl apply -f k8s/tw-tasks.yaml
 # ingress siempre debemos ejecutarlo, pues este es el puente entre los servicios internos del Cluster en Kubernetes
 kubectl apply -f k8s/ingress.yaml
 
 # Exponer servicios
-kubectl port-forward service/tw-shell-service 8080:80
-kubectl port-forward service/tw-auth-service 8081:80
-kubectl port-forward service/tw-chat-service 8082:80
+kubectl port-forward service/tw-shell-service 8080:80 &
+kubectl port-forward service/tw-auth-service 8081:80 &
+kubectl port-forward service/tw-chat-service 8082:80 &
+kubectl port-forward service/tw-docs-service 8083:80 &
+kubectl port-forward service/tw-tasks-service 8084:80
